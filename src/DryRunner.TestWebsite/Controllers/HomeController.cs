a9d2223f1b4e63a32edb362b969ab6313a8f846d
Tim Jones <tim@timjones.tw>
2013-04-30 23:54:20 +0800

    Add test website

---
+++ src/DryRunner.TestWebsite/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DryRunner.TestWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/DryRunner.TestWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DryRunner.TestWebsite.MvcApplication" Language="C#" %>
